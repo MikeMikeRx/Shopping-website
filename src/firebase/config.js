55627eb7f3ff99692070fc205f34b9d7e9ce49b1
MichalRais <Michal.Rais@iCloud.com>
2025-07-30 15:23:49 +0100

    Initialize Firebase in config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4J53VkPSFdepCPUnRA4meIVDp5sPRhes",
+  authDomain: "shopping-web-19122.firebaseapp.com",
+  projectId: "shopping-web-19122",
+  storageBucket: "shopping-web-19122.firebasestorage.app",
+  messagingSenderId: "396745026564",
+  appId: "1:396745026564:web:1cf9c0f366394018a41951"
+};
+
+const app = initializeApp(firebaseConfig);
