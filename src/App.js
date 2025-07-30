ffa0223d49a81e705cebbf3d240ab84a324adcb2
MichalRais <Michal.Rais@iCloud.com>
2025-07-30 11:11:57 +0100

    Add BrowserRouter and Routes to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./pages/Home"
 import Products from "./pages/Products"
 import ProductDetail from "./pages/ProductDetail"
-import SharedLayout from "./pages/SharedLayout"
+import SharedLayout from "./components/SharedLayout"
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={ <SharedLayout /> }>
+        <Route index element={ <Home /> }/>
+        <Route path="Products" element={ <Products /> }/>
+        <Route path="ProductDetail" element={ <ProductDetail />}/>
+      </Route>
+    </Routes>  
+  </BrowserRouter>
+  
 }
 
 export default App
--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -1,6 +1,5 @@
-import Navigation from "../components/Navigation"
-import Footer from "../components/Footer"
-
+import Navigation from "./Navigation"
+import Footer from "./Footer"
 import { Outlet } from "react-router-dom"
 
 
