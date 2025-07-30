9da05f1dba9d8e97387a60dd0b4df8893e31277f
MichalRais <Michal.Rais@iCloud.com>
2025-07-30 10:59:32 +0100

    Import Navigation.js and Footer.js to SharedLayout

--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,4 +1,8 @@
+import Navigation from "../components/Navigation"
+import Footer from "../components/Footer"
+
 import { Outlet } from "react-router-dom"
+
 
 const SharedLayout = () => {
   return (
