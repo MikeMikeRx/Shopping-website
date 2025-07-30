43d8c4763c0d76da9b30ae54b6294c247923929c
MichalRais <Michal.Rais@iCloud.com>
2025-07-30 10:52:35 +0100

    Import pages to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Products from "./pages/Products"
+import ProductDetail from "./pages/ProductDetail"
+import SharedLayout from "./pages/SharedLayout"
 
 const App = () => {
   return (
