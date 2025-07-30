982e387b84c55742c3ccee35ea9ba13207e1c6c4
MichalRais <Michal.Rais@iCloud.com>
2025-07-30 12:04:20 +0100

    Add responsive navigation with toggle menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,32 @@
 import "./Navigation.css"
-import { GitHamburgerMenu } from "react-icons/gi"
+import { GiHamburgerMenu } from "react-icons/gi"
 import { useState } from "react"
 import navLinks from "../navLinks"
 
 const Navigation = () => {
-  const [showMenu, setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(true) 
 
-  return <div>
-    <h1>AMAZING SHOP</h1>
-    <nav>
-      
-    </nav>
-  </div> 
+  return <nav>
+    <div className="nav-header">
+      <h1>AMAZING SHOP</h1>
+      <button onClick={ ()=> setShowMenu(!showMenu) }>
+        <GiHamburgerMenu />
+      </button>
+    </div>
+    <div className={`${showMenu ? "nav-list show" : "nav-list hide"} `}>
+      <ul>
+        {
+          navLinks.map( (oneLink) => {
+            const {id, url, text} = oneLink
+            return <li key={id}>
+              <a href={url}>{text}</a>
+            </li>
+          })
+        }
+        </ul>
+    </div>
+  </nav>
+   
 }
 
 export default Navigation
