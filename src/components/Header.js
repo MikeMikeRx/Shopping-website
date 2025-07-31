746c5f61e4418cd7f35b0ed68ed285fc1326c097
MichalRais <Michal.Rais@iCloud.com>
2025-07-31 11:20:58 +0100

    Add scroll-based header hide/show behavior

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,35 @@
 import "./Header.css"
+import { useEffect, useRef, useState } from "react"
 
 const Header = () => {
-  return (
-    <div className="header">
-        <h1 >AMAZING SHOP</h1>
-    </div>
-  )
+  const [hide, setHide] = useState(false)
+  let lastScrollY = useRef(0)
+
+  useEffect( ()=>{
+    const mainContent = document.querySelector(".main-content")
+
+      if(!mainContent) return
+    
+      const handleScroll = () => {
+      const currentScroll = mainContent.scrollTop
+            
+      if (currentScroll > lastScrollY.current && currentScroll > 60) {
+        setHide(true)
+      } else {
+        setHide(false)
+      }
+
+      lastScrollY.current = currentScroll
+    }
+
+    mainContent.addEventListener("scroll", handleScroll)
+    return () => mainContent.removeEventListener("scroll", handleScroll)
+  },[])
+
+  return <header className={`header ${hide ? "hide-header" : ""}`}>
+    <h1 >AMAZING SHOP</h1>
+  </header>
+    
 }
 
 export default Header
