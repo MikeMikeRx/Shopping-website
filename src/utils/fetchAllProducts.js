a7c5bdae7a5ef6f85fe1eb31c48159c49e10a609
MichalRais <Michal.Rais@iCloud.com>
2025-08-05 13:31:01 +0100

    Add function to fetch all products from multiple collections

--- src/utils/fetchAllProducts.js
+++ src/utils/fetchAllProducts.js
@@ -1,10 +1,27 @@
 import { db } from "../firebase/config"
-import { doc, getDoc } from "firebase/firestore"
+import { collection, doc, getDoc } from "firebase/firestore"
 
-const fetchAllProducts = () => {
-  return (
-    <div>fetchAllProducts</div>
-  )
+const fetchAllProducts = async () => {
+    const allProducts = []
+
+    try {
+        for(const col of collectionsToFetch){
+            const colRef = collection(db, col)
+            const snapshot = await getDocs(colRef)
+
+            snapshot.forEach(doc => {
+                allProducts.push({
+                    id: doc.id,
+                    ...doc.data(),
+                    category: col,
+                })
+            })
+        }
+    } catch(err) {
+        console.log("Error.................!!!");        
+    }
+
+  return allProducts
 }
 
 export default fetchAllProducts
