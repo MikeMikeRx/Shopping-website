1633e24c0968ffe58379efda05f67f38111e1e5b
MichalRais <Michal.Rais@iCloud.com>
2025-08-05 09:18:53 +0100

    Import useEffect and fetch the data in ProductDeatails.js

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,13 +1,38 @@
 import "./ProductDetail.css"
 import { useParams } from "react-router-dom"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { db } from "../firebase/config"
 import { doc, getDoc } from "firebase/firestore"
 
 const ProductDetail = () => {
   const { productId } = useParams()
   const [product, setProduct] = useState(null)
   const [loading, setLoading] = useState(true)
-  const [error, setError] = useState("") 
+  const [error, setError] = useState("")
+  
+  useEffect ( () => {
+    if (!product) return
+
+    const docRef = doc(db, "electronics", productId)
+    
+    const fetchProduct = async () => {
+      try{
+        const docSnap = await getDoc(docRef)
+        if (docSnap.exists()) {
+          setProduct( { id: docSnap.id, ...docSnap.data() })
+          setError("")
+        } else {
+          setError("No product found")
+        }
+      } catch (err){
+        setError("Error fetching product details" + err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    
+    fetchProduct()
+  },[productId]) 
 
 
 
