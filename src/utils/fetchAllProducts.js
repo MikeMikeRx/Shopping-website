d224b6ac178562fb49a5bebee712ea66c2f4c6a0
MichalRais <Michal.Rais@iCloud.com>
2025-08-05 13:14:31 +0100

    Create fetchAllProducts.js

---
+++ src/utils/fetchAllProducts.js
@@ -0,0 +1,10 @@
+import { db } from "../firebase/config"
+import { doc, getDoc } from "firebase/firestore"
+
+const fetchAllProducts = () => {
+  return (
+    <div>fetchAllProducts</div>
+  )
+}
+
+export default fetchAllProducts
