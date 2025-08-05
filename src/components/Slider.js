f7f172db3439f3b5befce3c613fdf2824f6299ab
MichalRais <Michal.Rais@iCloud.com>
2025-08-05 13:00:50 +0100

    Import db from /config and doc,getDoc from firestore

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,9 @@
 import "./Slider.css"
+import { db } from "../firebase/config"
+import { doc, getDoc } from "firebase/firestore"
+import { useState } from "react"
+
+
 
 const Slider = () => {
   return (
