067412ddc397e8b53f5c1009926d2631cb33cfb0
MichalRais <Michal.Rais@iCloud.com>
2025-08-09 09:32:00 +0100

    Import and add Link

--- src/components/BotBar.js
+++ src/components/BotBar.js
@@ -1,9 +1,21 @@
 import "./BotBar.css"
+import { Link } from "react-router-dom"
 
 const BotBar = () => {
   return (
     <section>
-       <div>BotBar</div> 
+        <div>
+            <Link></Link>
+            <Link></Link>
+        </div>
+        <div>
+            <h3>AAAA</h3>
+            <p>Incidunt debitis, dolor quibusdam quaerat necessitatibus dicta, quod aliquid asperiores, aspernatur dolorum perspiciatis? Sit, molestias.</p>
+        </div>
+        <div>
+            <Link></Link>
+        </div>
+         
     </section>
     
   )
