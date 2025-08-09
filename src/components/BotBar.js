1c3e752df8751c80941ef6fbe912a4ddf26b4a2b
MichalRais <Michal.Rais@iCloud.com>
2025-08-09 11:10:15 +0100

    Style BotBar imgs and text

--- src/components/BotBar.js
+++ src/components/BotBar.js
@@ -1,19 +1,38 @@
 import "./BotBar.css"
 import { Link } from "react-router-dom"
+import img1 from "../images/main-page/bot1.jpg"
+import img2 from "../images/main-page/prew10.jpeg"
+import img3 from "../images/main-page/prew7.1.jpg"
+import img4 from "../images/main-page/bot6.jpg"
+import img5 from "../images/main-page/bot5.png"
+import img6 from "../images/main-page/bot7.jpg"
 
 const BotBar = () => {
   return (
-    <section>
-        <div>
-            <Link></Link>
-            <Link></Link>
+    <section className="bottom-bar">
+        <div className="sec-1">
+            <Link>
+                <img src={img4} alt="" />
+            </Link>
+            <p>Our sellers guarantee top quality and trusted brands. Enjoy great prices, exclusive deals, and reliable service. Shop confidently with us!</p>
+            <Link>
+                <img src={img2} alt="" />
+            </Link>
         </div>
-        <div>
-            <h3>AAAA</h3>
-            <p>Incidunt debitis, dolor quibusdam quaerat necessitatibus dicta, quod aliquid asperiores, aspernatur dolorum perspiciatis? Sit, molestias.</p>
+
+        <div className="sec-2">
+            <h3>Online shopping has never been easier.</h3>
+            <p>Enjoy hassle-free online shopping with our easy-to-use platform. Secure payments, fast checkout, and quick delivery make your experience smooth and convenient every time.</p>
+            <Link>
+                <img src={img6} alt="" />
+            </Link>
         </div>
-        <div>
-            <Link></Link>
+
+        <div className="sec-3">
+            <Link>
+                <img src={img3} alt="" />
+            </Link>
+            <h3>Shopp online from comfort of your home.</h3>
         </div>
          
     </section>
