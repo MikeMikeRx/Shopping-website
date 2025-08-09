b37f3bb32ba7bf917d0aa6eab4568b129aa2c923
MichalRais <Michal.Rais@iCloud.com>
2025-08-09 09:13:37 +0100

    Add styles to CatBar.css

--- src/components/CatBar.js
+++ src/components/CatBar.js
@@ -1,12 +1,12 @@
 import "./CatBar.css"
 import { Link } from "react-router-dom"
-import img1 from "../images/main-page/cat1.png"
+import img1 from "../images/main-page/cat3.jpg"
 import img2 from "../images/main-page/cat2.jpg"
 import img3 from "../images/main-page/cat5.jpg"
 
 const CatBar = () => {
   return (
-    <>
+    <section className="categories-section">
         <Link to="products?category=furniture">
           <img src={img3} alt="" />
         </Link>
@@ -18,7 +18,7 @@ const CatBar = () => {
         <Link to="products?category=clothes">
         <img src={img2} alt="" />
         </Link>
-    </>
+    </section>
   )
 }
 
