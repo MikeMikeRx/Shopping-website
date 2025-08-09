9e2f05577ac79f2cc9bb0b4ed418b0540f6d1503
MichalRais <Michal.Rais@iCloud.com>
2025-08-09 08:51:47 +0100

    Import and render IMGs in CatBar.js

--- src/components/CatBar.js
+++ src/components/CatBar.js
@@ -1,13 +1,23 @@
 import "./CatBar.css"
 import { Link } from "react-router-dom"
-import img1 from "..images/main-page/cat1.png"
+import img1 from "../images/main-page/cat1.png"
+import img2 from "../images/main-page/cat2.jpg"
+import img3 from "../images/main-page/cat5.jpg"
 
 const CatBar = () => {
   return (
     <>
-        <Link to="products?category=furniture">AAA</Link>
-        <Link to="products?category=electronics">BBB</Link>
-        <Link to="products?category=clothes">CCC</Link>
+        <Link to="products?category=furniture">
+          <img src={img3} alt="" />
+        </Link>
+        
+        <Link to="products?category=electronics">
+          <img src={img1} alt="" />
+        </Link>
+        
+        <Link to="products?category=clothes">
+        <img src={img2} alt="" />
+        </Link>
     </>
   )
 }
