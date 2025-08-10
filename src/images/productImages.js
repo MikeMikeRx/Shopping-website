99b58a7ade64a1ddf09626566b218f09133f6d2a
MichalRais <Michal.Rais@iCloud.com>
2025-08-10 12:19:37 +0100

    Create productImages and import IMGS to products

---
+++ src/images/productImages.js
@@ -0,0 +1,32 @@
+import tv1 from "./electronics/tv1.jpg"
+import tv2 from "./electronics/tv2.jpg"
+import tv3 from "./electronics/tv3.jpg"
+import tv4 from "./electronics/tv4.jpg"
+import lap1 from "./electronics/latop1.jpg"
+import lap2 from "./electronics/laptop2.jpg"
+import lap3 from "./electronics/laptop3.jpg"
+import lap4 from "./electronics/laptop4.jpg"
+import phone1 from "./electronics/mob1.jpg"
+import phone2 from "./electronics/mob2.jpg"
+
+
+const productImages =  {
+    electronics: { 
+        smartTv: {
+            tv1,
+            tv2,
+            tv3,
+            tv4,
+        }, laptops: {
+            lap1,
+            lap2,
+            lap3,
+            lap4,
+        }, smartPhones: {
+            phone1,
+            phone2,
+        }
+    },    
+}
+
+export default productImages
