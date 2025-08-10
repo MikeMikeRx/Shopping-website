17d2eee84c2749c1bba2c268c0e722fc7fa1ad83
MichalRais <Michal.Rais@iCloud.com>
2025-08-10 15:06:28 +0100

    Add path to Links in BotBar.js

--- src/components/Home/BotBar.js
+++ src/components/Home/BotBar.js
@@ -10,11 +10,11 @@ const BotBar = () => {
   return (
     <section className="bottom-bar">
         <div className="sec-1">
-            <Link className="img-1">
+            <Link to="products" className="img-1">
                 <img src={img1} alt="" />
             </Link>
             <p>Our sellers guarantee top quality and trusted brands. Enjoy great prices, exclusive deals, and reliable service. Shop confidently with us!</p>
-            <Link className="img-2">
+            <Link to="products?category=electronics" className="img-2">
                 <img src={img2} alt="" />
             </Link>
         </div>
@@ -22,13 +22,13 @@ const BotBar = () => {
         <div className="sec-2">
             <h3>Online shopping has never been easier.</h3>
             <p>Enjoy hassle-free online shopping with our easy-to-use platform. Secure payments, fast checkout, and quick delivery make your experience smooth and convenient every time.</p>
-            <Link>
+            <Link to="products">
                 <img src={img3} alt="" />
             </Link>
         </div>
 
         <div className="sec-3">
-            <Link>
+            <Link to="products?category=furniture">
                 <img src={img4} alt="" />
             </Link>
             <h3>Shopp online from comfort of your home.</h3>
