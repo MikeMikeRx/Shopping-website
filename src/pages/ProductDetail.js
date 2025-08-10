4120b1fd6247bf37a9fe9f059f2dfc0522d74df7
MichalRais <Michal.Rais@iCloud.com>
2025-08-10 14:46:11 +0100

    Add specific Img to the product

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -47,12 +47,10 @@ const ProductDetail = () => {
       {error && <p>{error}</p>}
       {product && (
         <div key={product.id}>
-
-
-          <img src={DummyImg} alt="" />
-
-
-          
+          <img 
+          src={productImages[product.img] || DummyImg} 
+          alt={product.name} 
+          />
           <div className="text">
           <h2>{product.name}</h2>          
           <p className="description">{product.description}</p>
