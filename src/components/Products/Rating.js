ba0ef64ce8a09331d946d56f3e5a3e8ef4a2ebd9
MichalRais <Michal.Rais@iCloud.com>
2025-08-12 12:22:32 +0100

    Add styles in Rating.css

--- src/components/Products/Rating.js
+++ src/components/Products/Rating.js
@@ -29,14 +29,12 @@ const Rating = ({ productId}) => {
     }
 
     
-  return <div>
+  return <div className="rating-bar">
     {[1,2,3,4,5].map((_, index) => (
       <button 
       key={index}
       ref={(el) => ratingRef.current[index] = el}
       onClick={() => handleRating(index)}
-      style={{ background: "none", border: "none", cursor: "pointer" }}
-      aria-label={`Rate ${index + 1} stars`}
       >
         <IoStar />
       </button>
