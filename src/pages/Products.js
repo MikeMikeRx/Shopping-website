5648541903f00d3a560e203ae4f96af2c0785a9f
MichalRais <Michal.Rais@iCloud.com>
2025-08-29 15:58:37 +0100

    Products.js Update: useEffect to handle URL query parameters

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -20,11 +20,16 @@ const Products = () => {
 
   useEffect(() => {
     const params = new URLSearchParams(location.search)
+
     const categoryFromQuery = params.get("category")
+    const isBestRated = params.get("bestRated") === "true"
 
     if (categoryFromQuery) {
       setSelectedCategories([categoryFromQuery])
     }
+
+    setIsBestRated(isBestRated)
+    
   }, [location.search, setSelectedCategories])
 
 
