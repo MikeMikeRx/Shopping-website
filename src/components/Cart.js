b490ef46d0571aed5b8b2aec8c64608c485ca4d4
MichalRais <Michal.Rais@iCloud.com>
2025-09-18 13:39:28 +0100

    feat(Cart): add message for empty cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,7 +2,9 @@ import { useOutletContext } from "react-router-dom"
 
 const Cart = () => {
   const { cart, removeFromCart } = useOutletContext()
-  
+
+  if (cart.length === 0) return <p>Shopping Basket is empty</p>
+
   return (
     <div>Card</div>
   )
